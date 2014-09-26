3ff28b23a89f6bd890f810d92a196668a10aa0cd
Michael Harvey <wharvey@mymail.mines.edu>
2014-09-26 11:52:27 -0600

    started project skeleton. Finished through BoardCell class

---
+++ src/clueGame/Board.java
@@ -0,0 +1,38 @@
+package clueGame;
+
+import java.util.Map;
+
+public class Board {
+	// instance variables
+	private BoardCell[][] grid;
+	private Map<Character, String> rooms;
+	private int numRows;
+	private int numCols;
+	
+	// loads the board layout
+	public void loadBoardConfig() {
+		
+	}
+	
+	// getters
+	public BoardCell[][] getGrid() {
+		return grid;
+	}
+
+	public Map<Character, String> getRooms() {
+		return rooms;
+	}
+
+	public int getNumRows() {
+		return numRows;
+	}
+
+	public int getNumCols() {
+		return numCols;
+	}
+	public BoardCell getBoardCell(int row, int col) {
+		return grid[row][col];
+	}
+	
+	
+}
---
+++ src/clueGame/ClueGame.java
@@ -0,0 +1,14 @@
+package clueGame;
+
+import java.util.Map;
+
+public class ClueGame {
+	// instance variables
+	private Map<Character, String> rooms;
+	
+	// ?
+	public void loadConfig() {
+		
+	}
+
+}
