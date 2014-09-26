8004dcc666bd6b535e488c388023d03b9ad385fa
Michael Harvey <wharvey@mymail.mines.edu>
2014-09-26 15:26:27 -0600

    forgot to save RoomCell class

--- src/clueGame/RoomCell.java
+++ src/clueGame/RoomCell.java
@@ -1,6 +1,17 @@
 package clueGame;
 
 public class RoomCell extends BoardCell {
+	public enum DoorDirection {
+		UP, DOWN, RIGHT, LEFT
+	}
+	
+	private DoorDirection doorDirection;
+	private char roomInitial;
+	
+	@Override
+	public boolean isRoom() {
+		return true;
+	}
 
 	@Override
 	public void draw() {
