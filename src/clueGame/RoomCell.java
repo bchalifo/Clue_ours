2e0e84a71a61d5a11c1298bbc6b331ee5aed8c5e
Michael Harvey <wharvey@mymail.mines.edu>
2014-09-26 15:00:29 -0600

    'finished' skeleton for clueGame package

---
+++ src/clueGame/RoomCell.java
@@ -0,0 +1,11 @@
+package clueGame;
+
+public class RoomCell extends BoardCell {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/clueGame/WalkwayCell.java
@@ -0,0 +1,15 @@
+package clueGame;
+
+public class WalkwayCell extends BoardCell {
+	@Override
+	public boolean isWalkway() {
+		return true;
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
